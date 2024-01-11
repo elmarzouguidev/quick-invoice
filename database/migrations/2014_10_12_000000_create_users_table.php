27d2d1b718d4ef0e8691562ec943f21e7b03591b
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-01-11 16:54:35 +0100

    install Livewire 3

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nom', 20);
-            $table->string('prenom', 20);
+            $table->string('nom', 30);
+            $table->string('prenom', 30);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
