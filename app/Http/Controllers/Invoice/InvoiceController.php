43d6610ca41c5574c6a6ea083420b584a3f687f9
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-11-02 17:12:29 +0100

    start implement ReactJs views with vite

--- app/Http/Controllers/Invoice/InvoiceController.php
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers\Invoice;
 
 use App\Http\Controllers\Controller;
 use App\Models\Finance\Sell\Invoice;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class InvoiceController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    /*public function __invoke(Request $request)
     {
         $invoices = Invoice::all();
 
         return view('Invoice.index', compact('invoices'));
+    }*/
+
+    public function __invoke(User $user)
+    {
+        return Inertia::render('User/Show', [
+            'user' => $user
+        ]);
     }
 }
